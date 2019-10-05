21d83015d32d20d79864e3d7d287d576364c05fd
murillofr <murillo.falcao@gmail.com>
2019-10-05 15:02:15 -0300

    Managing product categories

--- src/main/java/com/example/course/resources/ProductResource.java
+++ src/main/java/com/example/course/resources/ProductResource.java
@@ -1,6 +1,7 @@
 package com.example.course.resources;
 
 import java.net.URI;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.course.dto.CategoryDTO;
 import com.example.course.dto.ProductCategoriesDTO;
 import com.example.course.dto.ProductDTO;
 import com.example.course.services.ProductService;
@@ -83,4 +85,24 @@ public class ProductResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}/addcategory")
+	public ResponseEntity<Void> addCategory(@PathVariable Long id, @RequestBody CategoryDTO dto) {
+		service.addCategory(id, dto);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}/removecategory")
+	public ResponseEntity<Void> removeCategory(@PathVariable Long id, @RequestBody CategoryDTO dto) {
+		service.removeCategory(id, dto);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}/setcategories")
+	public ResponseEntity<Void> setCategories(@PathVariable Long id, @RequestBody List<CategoryDTO> dto) {
+		service.setCategories(id, dto);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/example/course/services/ProductService.java
+++ src/main/java/com/example/course/services/ProductService.java
@@ -98,4 +98,27 @@ public class ProductService {
 		return products.map(e -> new ProductDTO(e));
 	}
 	
+	@Transactional
+	public void addCategory(Long id, CategoryDTO dto) {
+		Product product = productRepository.getOne(id);
+		Category category = categoryRepository.getOne(dto.getId());
+		product.getCategories().add(category);
+		productRepository.save(product);
+	}
+	
+	@Transactional
+	public void removeCategory(Long id, CategoryDTO dto) {
+		Product product = productRepository.getOne(id);
+		Category category = categoryRepository.getOne(dto.getId());
+		product.getCategories().remove(category);
+		productRepository.save(product);
+	}
+	
+	@Transactional
+	public void setCategories(Long id, List<CategoryDTO> dto) {
+		Product product = productRepository.getOne(id);
+		setProductCategories(product, dto);
+		productRepository.save(product);
+	}
+	
 }
